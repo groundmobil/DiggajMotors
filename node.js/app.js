9f26d6b30a917d4f153b368e1564ea1b03269b9d
groundmobil <152173861+groundmobil@users.noreply.github.com>
2024-02-05 14:34:54 +0530

    Add files via upload

---
+++ node.js/app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://diggajmotors.s3-website.ap-south-1.amazonaws.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+ 
+  
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/DiggajMotorsDB', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connection successful');
+        app.listen(PORT, () => {
+            console.log('Server is running on http://localhost:${PORT}');
+        });
+    })
+    .catch((error) => {
+        console.error('MongoDB connection failed:', error);
+    });
+
+const customerSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    MobileNumber: {
+        type: Number, 
+        required: true,
+        unique: true
+    },
+    City: {
+        type: String,
+        required: true,
+        enum: ['Ahmedabad', 'Amravati', 'Anand', 'Aurangabad', 'Bengaluru', 'Bharuch', 'Bhavnagar', 'Chennai', 'Delhi', 'Gandhinagar', 'Hyderabad', 'Jaipur', 'Jamnagar', 'Junagadh', 'Kolhapur', 'Kolkata', 'Lucknow', 'Mumbai', 'Nagpur', 'Nashik', 'Noida', 'Pune', 'Rajkot', 'Solapur', 'Surat', 'Thane', 'Vadodra']
+    },
+    Interest: {
+        type: String,
+        required: true,
+        enum: ['Product Enquiry', 'Investment', 'EV Reporter', 'Career']
+    }
+}, {
+    timestamps: true
+});
+
+const Customers = mongoose.model('customer', customerSchema);
+
+
+
+app.post('/submit-form', async (req, res) => {
+    const formData = req.body;
+    console.log(req)
+    try {
+        console.log('Received form data:', formData);
+
+        const savedForm = await Customers.create(formData);
+        console.log('Saved form:', savedForm);
+
+        res.json({ success: true, message: 'Form submitted successfully' });
+    } catch (error) {
+        console.error('Error during form submission:', error);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+});
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+module.exports = Customers;
